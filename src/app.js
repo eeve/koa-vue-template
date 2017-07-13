08bae4211f39cb0e684d8f1d84b9863b55c97448
eeve <eeveme@gmail.com>
2017-07-13 15:09:38 +0800

    fix favicon

--- src/app.js
+++ src/app.js
@@ -27,7 +27,7 @@ const pug = new Pug({
 const serve = (path, cache) => Serve(resolve(path), {
 	maxage: cache && isProd ? 1000 * 60 * 60 * 24 * 30 : 0
 });
-server.use(favicon('../public/logo-48.png'));
+server.use(favicon(resolve('./favicon.ico')));
 server.use(serve('../public', true));
 
 if(!isProd) {
