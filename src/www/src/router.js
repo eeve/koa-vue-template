79fe8ccee98b5ee792b24f3ddcf38bda12712533
eeve <eeveme@gmail.com>
2017-08-06 15:08:41 +0800

    fix router

--- src/www/src/router.js
+++ src/www/src/router.js
@@ -5,7 +5,7 @@ Vue.use(Router);
 export default new Router({
 	mode: 'history',
 	scrollBehavior: () => ({ y: 0 }),
-	router: [
+	routes: [
 		{ path: '/index', meta: { title: '首页' }, component (resolve) { import('./components/Home.vue').then(resolve); } }
 	]
 });
